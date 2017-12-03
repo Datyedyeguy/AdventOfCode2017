9c145a5ae5886941921fcef4301accbc9280dbe7
Hans Brigman <hans@datyedyeguy.net>
2017-12-02 20:58:07 -0500

    Day 1: Star 2 Complete

--- 1/main.lua
+++ 1/main.lua
@@ -3,7 +3,8 @@ data = file:read()
 file:close()
 
 sum = 0
-i = data:len()
+l = data:len()
+i = l
 last = data:sub(1,1)
 
 while (i >= 0) do
@@ -17,4 +18,26 @@ while (i >= 0) do
 	i = i - 1
 end
 
-print(sum)
+print("Part 1: " .. sum)
+
+sum = 0
+i = 0
+half = l/2;
+
+while (i <= l) do
+	curr = data:sub(i,i)
+
+	if (i < half) then
+		last = data:sub(i+half, i+half)
+	else
+		last = data:sub(i-half, i-half)
+	end
+
+	if (curr == last) then
+		sum = sum + curr
+	end
+
+	i = i + 1
+end
+
+print("Part 2: " .. sum)
