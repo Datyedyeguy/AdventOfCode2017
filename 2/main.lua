0de2ecbfa874a70a269c0ccb18a2402ddceb90d8
Hans Brigman <hans@datyedyeguy.net>
2017-12-02 21:16:25 -0500

    Day 2: Part 2 Complete

--- 2/main.lua
+++ 2/main.lua
@@ -1,5 +1,4 @@
 file = io.open("input", "r")
-
 sum = 0
 
 for data in file:lines() do
@@ -8,7 +7,7 @@ for data in file:lines() do
 
 	for i in data:gmatch("%S+") do
 		i = tonumber(i)
-		
+
 		if (i > max) then
 			max = i
 		end
@@ -23,3 +22,36 @@ end
 
 file:close()
 print("Part 1: " .. sum)
+
+file = io.open("input", "r")
+sum = 0
+
+for data in file:lines() do
+	numbers = {}
+
+	for i in data:gmatch("%S+") do
+		table.insert(numbers, tonumber(i))
+	end
+
+	for i = 1, #numbers do
+		for j = i+1, #numbers do
+			first = numbers[i]
+			second = numbers[j]
+
+			if (first < second) then
+				temp = first
+				first = second
+				second = temp
+			end
+
+			result = math.fmod(first, second)
+
+			if (result == 0) then
+				sum = sum + (first / second)
+				print(first .. ":" .. second .. "|" .. result)
+			end
+		end
+	end
+end
+
+print("Part 2: " .. sum)
