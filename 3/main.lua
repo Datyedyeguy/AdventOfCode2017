9ed8978b1ef8a8394932700165bfb7e1cc0a6441
Hans Brigman <hans@datyedyeguy.net>
2017-12-03 21:36:20 -0500

    Day 3: Part 2 Complete

--- 3/main.lua
+++ 3/main.lua
@@ -2,8 +2,6 @@ ring = 0
 square = 1
 goal = 325489
 
-print("goal: " .. goal)
-
 while (square * square < goal) do
 	square = square + 2;
 	ring = ring + 1
@@ -32,3 +30,81 @@ while (test < goal) do
 end
 
 print("Part 1: " .. step)
+
+data = {}
+x = 1
+y = 0
+data["0,0"] = 1
+last = 1
+dir = "U"
+
+while (last < goal) do
+	temp = 0
+
+	index = (x-1)..","..(y-1)
+	if (data[index]) then
+		temp = temp + data[index]
+	end
+	index = (x-1)..","..(y-0)
+	if (data[index]) then
+		temp = temp + data[index]
+	end
+	index = (x-1)..","..(y+1)
+	if (data[index]) then
+		temp = temp + data[index]
+	end
+	index = (x-0)..","..(y-1)
+	if (data[index]) then
+		temp = temp + data[index]
+	end
+	index = (x-0)..","..(y+1)
+	if (data[index]) then
+		temp = temp + data[index]
+	end
+	index = (x+1)..","..(y-1)
+	if (data[index]) then
+		temp = temp + data[index]
+	end
+	index = (x+1)..","..(y-0)
+	if (data[index]) then
+		temp = temp + data[index]
+	end
+	index = (x+1)..","..(y+1)
+	if (data[index]) then
+		temp = temp + data[index]
+	end
+
+	data[x..","..y] = temp
+	last = temp
+
+	if (dir == "U") then
+		if (x == y) then
+			dir = "L"
+			x = x - 1
+		else
+			y = y + 1
+		end
+	elseif (dir == "L") then
+		if (-x == y) then
+			dir = "D"
+			y = y - 1
+		else
+			x = x - 1
+		end
+	elseif (dir == "D") then
+		if (x == y) then
+			dir = "R"
+			x = x + 1
+		else
+			y = y - 1
+		end
+	elseif (dir == "R") then
+		if (x == -y) then
+			dir = "U"
+		end
+
+		x = x + 1
+	end
+end
+
+print("Part 2: " .. last)
