b367715f9603ebd28e06978988b82c10c934a3b1
Hans Brigman <hans@datyedyeguy.net>
2017-12-05 20:57:29 -0500

    Day 5: Part 2 Complete

--- 5/main.lua
+++ 5/main.lua
@@ -5,6 +5,8 @@ for line in file:lines() do
 	table.insert(data, line)
 end
 
+file:close();
+
 index = 1
 steps = 0
 
@@ -15,5 +17,31 @@ while (index > 0 and index <= #data) do
 	steps = steps + 1
 end
 
-file:close();
 print("Part 1: " .. steps)
+
+file = io.open("input", "r")
+data = {}
+
+for line in file:lines() do
+	table.insert(data, line)
+end
+
+file:close();
+
+index = 1
+steps = 0
+
+while (index > 0 and index <= #data) do
+	movement = tonumber(data[index])
+
+	if (movement > 2) then
+		data[index] = movement - 1
+	else
+		data[index] = movement + 1
+	end
+	
+	index = index + movement
+	steps = steps + 1
+end
+
+print("Part 2: " .. steps)
