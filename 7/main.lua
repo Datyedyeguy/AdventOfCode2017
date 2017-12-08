2f304be30418860aa4f2989aec80588fc0f1103a
Hans Brigman <hans@datyedyeguy.net>
2017-12-07 22:30:40 -0500

    Day 7: Part 2 Complete

--- 7/main.lua
+++ 7/main.lua
@@ -2,14 +2,14 @@ file = io.open("input", "r")
 disks = {}
 
 for line in file:lines() do
-	disk = {branches = {}, is_bottom = true}
+	disk = {branches = {}, is_bottom = true, goal_weight = 0}
 	branches = {}
 
 	for part in line:gmatch("%S+") do
 		if disk["id"] == nil then
 			disk.id = part
 		elseif disk["weight"] == nil then
-			disk.weight = part:sub(2,(#part)-1)
+			disk.weight = tonumber(part:sub(2,(#part)-1))
 		elseif part ~= "->" then
 			part = string.gsub(part,",","") 
 			table.insert(disk.branches, part)
@@ -30,11 +30,74 @@ for id, disk in pairs(disks) do
 end
 
 result = ""
+goal_weight = -1
+
+function check_weight(disk)
+	if (#disk.branches == 0) then
+		return disk.weight
+	end
+
+	local total = disk.weight
+	local branch_weights = {}
+
+	for b=1, #disk.branches do
+		table.insert(branch_weights, check_weight(disks[disk.branches[b]]))
+	end
+
+	local min = 99999
+	local max = -1
+
+	for b=1, #branch_weights do
+		if min > branch_weights[b] then
+			min = branch_weights[b]
+		end
+		if max < branch_weights[b] then
+			max = branch_weights[b]
+		end
+	end
+
+	if min ~= max then
+		-- We found the error!  Find which branch should be adjusted
+		local min_count = 0
+		local max_count = 0
+
+		for b=1, #branch_weights do
+			if (branch_weights[b] == min) then
+				min_count = min_count + 1
+			else
+				max_count = max_count + 1
+			end
+		end
+
+		local goal = 0
+		local diff = 0
+
+		if (min_count == 1) then
+			goal = min
+			diff = max - min
+		else
+			goal = max
+			diff = min - max
+		end
+
+		for b=1, #branch_weights do
+			if (branch_weights[b] == goal) then
+				goal_weight = disks[disk.branches[b]].weight + diff
+				b = #branch_weights
+			end
+		end
+	end
+
+	return total + (min * #branch_weights)
+end
 
 for id, disk in pairs(disks) do
 	if (disk.is_bottom) then
 		result = id
+		check_weight(disk)
 	end
 end
 
 print("Part 1: " .. result)
+
+print("Part 2: " .. goal_weight)
